140de4ca10fcc8542a189a948b2d72d32439c34a
bhwi0015 <yy.bhwi0015@softbank.ne.jp>
2021-05-26 18:22:37 +0900

    ユーザー管理機能実装

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,23 +1,17 @@
 class Product < ApplicationRecord
 
-    devise :database_authenticatable, :registerable,
-    :recoverable, :rememberable, :validatable
 
-    validates :nickname, presence: true
-    validates :birthday, presence: true
+  belongs_to :user 
+  has_one_attached :image
+  
+  validates :product_name,  presence: true
+  validates :description,   presence: true
+  validates :category_id,   presence: true
+  validates :status_id,     presence: true
+  validates :shipping_id,   presence: true
+  validates :prefecture_id, presence: true
+  validates :send_day_id,   presence: true
+  validates :price,         presence: true
+  validates :image,         presence: true
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'に全角文字を使用してください' } do
-     validates :last_name_kanji
-     validates :first_nanme_kanji
-    end
-
-  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'に全角カタカナを使用してください' } do
-     validates :last_name_kana
-     validates :first_name_kana
-    end
-
-    def password_complexity
-     return if password.blank? || password =~ /^(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}$/i
-   errors.add :password, "パスワードの強度が不足しています。半角英数字混合で入力する必要があります。"
-  end 
-end
+end  
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,26 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+    devise :database_authenticatable, :registerable,
+    :recoverable, :rememberable, :validatable
+
+    validates :nickname, presence: true
+    validates :birthday, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'に全角文字を使用してください' } do
+     validates :last_name_kanji
+     validates :first_name_kanji
+    end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'に全角カタカナを使用してください' } do
+     validates :last_name_kana
+     validates :first_name_kana
+    end
+
+    def password_complexity
+     return if password.blank? || password =~ /^(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}$/i
+     errors.add :password, "パスワードの強度が不足しています。半角英数字混合で入力する必要があります。"
+    
+  end
 end
