bca0ba2a58e4ea09c181597f4497de5e8fd31fd8
bhwi0015 <yy.bhwi0015@softbank.ne.jp>
2021-05-27 19:37:44 +0900

    herokuエラー修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,18 +1,18 @@
-#class ProductsController < ApplicationController
- # def index
- #   @products = Product.all
- # end
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
 
- # def new
- #   @product = Product.new
- # end
+  def new
+    @product = Product.new
+  end
 
-#  def create
-#    @product = Product.create(product_params)
-#  end
+  def create
+    @product = Product.create(product_params)
+  end
 
-#  private
-#  def product_params
- #   params.require(:product).permit(:product_name, :description, :category_id, :status_id, :shipping_id, :prefecture_id, :send_day_id, :price, :image).merge(user_id: current_user.id)
- # end
-#end
+  private
+  def product_params
+    params.require(:product).permit(:product_name, :description, :category_id, :status_id, :shipping_id, :prefecture_id, :send_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+end
