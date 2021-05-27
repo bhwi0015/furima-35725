41f8eac7ba51317819f21d9e4ace126dd14e37ec
bhwi0015 <yy.bhwi0015@softbank.ne.jp>
2021-05-27 16:48:54 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-    devise :database_authenticatable, :registerable,
-    :recoverable, :rememberable, :validatable
-
     with_options presence: true do
       validates :nickname
       validates :birthday
